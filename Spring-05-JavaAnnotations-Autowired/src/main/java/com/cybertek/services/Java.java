7044c0aaef175290805f358a58cfc1297e6de3dc
Dianna Ryumina <dianna.ryumina@gmail.com>
2020-11-17 20:19:23 -0600

    Java Annotations - Constructor, Setter and Field Injections

--- Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -1,15 +1,37 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
+    //Field Injection
+    @Autowired
     private OfficeHours officeHours;
+
+//
+//    //Constructor Injection - need to create constructor
+//    @Autowired
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+    public OfficeHours getOfficeHours(){
+        return officeHours;
+    }
+
+//
+//    //Setter Injection - need Setter
+//@Autowired
+//    public void setOfficeHours(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
 
     @Override
     public void getTeachingHours() {
+
         System.out.println("Weekly teaching hourse: " + (30 + officeHours.getHours()));
     }
 }
