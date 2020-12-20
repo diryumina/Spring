51ce4b5f9d32e783a3b9c8e81262e74693e59b02
Dianna Ryumina <dianna.ryumina@gmail.com>
2020-12-20 17:20:14 -0600

    MVC Form Employee Lab

---
+++ Spring-08-SprinBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.cybertek.controller;
+
+import com.cybertek.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/register")
+    public String employeeCreate(Model model){
+        model.addAttribute("employee", new Employee());
+
+        return "/employee/employee-create";
+    }
+
+}
