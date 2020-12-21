5bca61960adc083a5a027d3a2e68eb3d18baa2ed
Dianna Ryumina <dianna.ryumina@gmail.com>
2020-12-20 18:39:04 -0600

    MVC Form Employee List html added

--- Spring-08-SprinBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
+++ Spring-08-SprinBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -1,10 +1,16 @@
 package com.cybertek.controller;
 
+import com.cybertek.dataGenerator.DataGenerator;
 import com.cybertek.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.Arrays;
 
 
 @Controller
@@ -14,8 +20,19 @@ public class EmployeeController {
     @GetMapping("/register")
     public String employeeCreate(Model model){
         model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getStateList());
 
         return "/employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String employeeList(@ModelAttribute("employee") Employee employee, Model model){
+
+        model.addAttribute("employeeList", Arrays.asList(employee) );
+
+        int birthYear = LocalDate.parse(employee.getBirthday()).getYear();
+        model.addAttribute("age", LocalDate.now().getYear()-birthYear);
+        return "employee/employee-list";
     }
 
 }
